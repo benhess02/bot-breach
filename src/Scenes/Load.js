d1a65aad651f12fefbeccd4a67861996be4f8114
Ben Hess <pilotben@comcast.net>
2024-06-12 02:18:10 -0700

    Refactoring and clean up.

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -6,34 +6,21 @@ class Load extends Phaser.Scene {
     preload() {
         this.load.setPath("./assets/");
 
-        // // Load townsfolk
-        // this.load.image("purple", "purple_townie.png");
-        // this.load.image("blue", "blue_townie.png");
+        this.load.image("tilesheet", "tilesheet.png");
+        this.load.tilemapTiledJSON("tilemap", "tilemap.tmj");
 
-        // // Load tilemap information
-        // this.load.image("tilemap_tiles", "tilemap_packed.png");                   // Packed tilemap
-        // this.load.tilemapTiledJSON("three-farmhouses", "three-farmhouses.tmj");   // Tilemap in JSON
-
-        this.load.image("tilesheet", "tilesheet.png");                   // Packed tilemap
-        this.load.tilemapTiledJSON("tilemap", "tilemap.tmj");   // Tilemap in JSON
-
-        this.load.image("robots", "robots.png");                   // Packed tilemap
+        this.load.image("robots", "robots.png");
         this.load.spritesheet("robots_sheet", "robots.png", {
             frameWidth: 62,
             frameHeight: 64,
             spacing: 10
         });
-
     }
 
     create() {
-        
-
-         // ...and pass to the next Scene
-         this.scene.start("pathfinderScene");
+         this.scene.start("levelScene");
     }
 
-    // Never get here since a new scene is started in create()
     update() {
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,27 +1,8 @@
-// Jim Whitehead
-// Created: 5/26/2024
-// Phaser: 3.80.0
-//
-// Pathfinder demo
-//
-// An example of pathfinding in Phaser using the EasyStar.js pathfinder 
-// https://github.com/prettymuchbryce/easystarjs
-// 
-// Assets from the following Kenney Asset packs
-// Tiny Dungeon
-// https://kenney.nl/assets/tiny-dungeon
-//
-// Tiny Town
-// https://kenney.nl/assets/tiny-town
-//
-
-
-// game config
 let config = {
     parent: 'phaser-game',
     type: Phaser.CANVAS,
     render: {
-        pixelArt: true  // prevent pixel art from getting blurred when scaled
+        pixelArt: true
     },
     physics: {
         default: 'arcade',
@@ -34,7 +15,7 @@ let config = {
     },
     width: 1280,
     height: 800,
-    scene: [Load, Pathfinder]
+    scene: [Load, Level]
 }
 
 var cursors;
